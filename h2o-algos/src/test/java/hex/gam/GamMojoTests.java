fda277cdb6bd1827d62b4d9e9fe5533282ea708f
wendycwong <wendycwong@yahoo.com>
2020-06-19 11:41:05 -0700

    added junit mojo gam test.

---
+++ h2o-algos/src/test/java/hex/gam/GamMojoTests.java
@@ -0,0 +1,12 @@
+package hex.gam;
+
+import org.junit.runner.RunWith;
+import water.TestUtil;
+import water.runner.CloudSize;
+import water.runner.H2ORunner;
+
+@RunWith(H2ORunner.class)
+@CloudSize(1)
+public class GamMojoTests extends TestUtil {
+  
+}
